55b0fa41124c8b18086433ad936794d56620a66c
Kummey <76731996+Kummey@users.noreply.github.com>
2021-03-11 11:39:29 +0100

    show played card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,12 +8,13 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
   game: Game;
   constructor() {}
 
   ngOnInit(): void {
     this.newGame();
-    console.log(this.game)
+ 
   }
 
   newGame() {
@@ -21,6 +22,16 @@ export class GameComponent implements OnInit {
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+     
+      console.log('New Card:',  this.currentCard);
+      console.log('Game is:' , this.game);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard)
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
 }
