150aef3cead8cb08eed4312c93450ff1c0bf47f9
Kummey <alexander@thomas-kummerer.de>
2021-03-15 09:46:08 +0100

    add game rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import { GameInfoComponent } from './game-info/game-info.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import {MatInputModule} from '@angular/material/input';
     StartScreenComponent,
     GameComponent,
     PlayerComponent,
-    AddPlayerDialogComponent
+    AddPlayerDialogComponent,
+    GameInfoComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +34,7 @@ import {MatInputModule} from '@angular/material/input';
     MatDialogModule,
     MatFormFieldModule,
     FormsModule,
+    MatCardModule,
     MatFormFieldModule,
     MatInputModule,
   ],
